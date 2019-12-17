62f2320421d5a9f5b8417fa4c6f8fdd4971c1b88
manuelatrotta <manuelat88@hotmail.it>
2019-12-17 15:47:13 +0100

    inserimento nome utente

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 alert("click ok for visit this page!");
+//chiedere nome all'utente
+var nomeUtente = prompt ('inserire nome');
+console .log (nomeUtente);
+//chiedere cognome
+//generare pass con nomecognomecolore19
