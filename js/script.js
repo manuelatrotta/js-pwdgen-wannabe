938caa309cdd94d4fb4a09de5bc637f813b23922
manuelatrotta <manuelat88@hotmail.it>
2019-12-17 15:58:55 +0100

    inserimento colore

--- js/script.js
+++ js/script.js
@@ -2,5 +2,11 @@ alert("click ok for visit this page!");
 //chiedere nome all'utente
 var nomeUtente = prompt ('inserire nome');
 console .log (nomeUtente);
+document .getElementById('nome') .innerHTML ="benvenuto" + nomeUtente;
 //chiedere cognome
+var cognomeUtente = prompt ('inserire cognome');
+console .log (cognomeUtente);
 //generare pass con nomecognomecolore19
+var colorePreferito = prompt ('inserisci il tuo colore preferito');
+console .log (colorePreferito);
+document .getElementById('colore') .innerHTML ="Questo è il tuo colore preferito: " + colorePreferito;
